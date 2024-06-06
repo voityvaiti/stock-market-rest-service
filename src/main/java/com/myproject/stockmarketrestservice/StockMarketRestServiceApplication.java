4839d6cfee70ae85519852b5dc3aa3e212f33dbf
voityvaiti <flashlight@i.ua>
2024-06-06 18:32:49 +0300

    Init

---
+++ src/main/java/com/myproject/stockmarketrestservice/StockMarketRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.stockmarketrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/stockmarketrestservice/StockMarketRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.stockmarketrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
